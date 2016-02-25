89aa19999eae53694ccba1a49843ce89dc8a1d6a
Jonathan Felton <j.eliasfelton@gmail.com>
2016-02-24 19:12:54 -0600

    Added Responsive Nav using JQuery

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $( ".menu-btn" ).click(function() {
-    $( ".hide-small" ).toggle();
+  var mButton = $(".menu-btn")
+  menu = $(".hide-small");
+  $( mButton ).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
     });
+  $(window).resize(function(){
+    		var w = $(window).width();
+    		if(w > 799 && menu.is(':hidden')) {
+    			menu.removeAttr('style');
+    		}
+  	});
+
 });
